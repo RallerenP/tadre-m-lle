c2eeb0b7ccf20cd6b581f79a4f8aacfa947b734f
Rasmus Povlsen <git@rpovlsen.com>
2020-11-24 15:53:13 +0100

    Began creating components

---
+++ src/main/resources/static/components/tadre-footer/tadre-footer.js
@@ -0,0 +1,14 @@
+import { html, define, children } from 'https://unpkg.com/hybrids@4.3.3/src/index.js';
+import { TadreCopyright } from '../tadre-copyright/tadre-copyright.js'
+
+export const TadreFooter = {
+    copyright: children(TadreCopyright),
+    src: "",
+    render: ({src, copyright}) => html`
+        <link href="/css/tailwind.min.css" rel="stylesheet">
+        <link href="/css/custom.css" rel="stylesheet">
+        <slot name="copyright" />
+    `
+}
+
+define('tadre-footer', TadreFooter);
