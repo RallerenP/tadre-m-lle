2d23a7825094880b2d3ee478ece712554424ef3b
Rasmus Povlsen <git@rpovlsen.com>
2021-01-22 17:33:47 +0100

    Quick Auth Removal for signup

--- src/main/java/com/mchg/tadremoelle/controllers/api/AuthController.java
+++ src/main/java/com/mchg/tadremoelle/controllers/api/AuthController.java
@@ -47,7 +47,6 @@ public class AuthController {
         else return "None";
     }
 
-    @AuthGuard
     @PostMapping("/signup")
     public boolean signup(@Valid @RequestBody AddUserDTO dto) {
         return this.authService.signUp(dto);
